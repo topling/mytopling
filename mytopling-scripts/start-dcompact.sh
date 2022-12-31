eec24da5e1d9dce99ea706aa0853712147e09c68
leipeng <peng@topling.cn>
2022-12-31 15:58:32 +0800

    Add mytopling-scripts/mytopling-enterprise.json

--- mytopling-scripts/start-dcompact.sh
+++ mytopling-scripts/start-dcompact.sh
@@ -8,6 +8,7 @@ export MULTI_PROCESS=1
 
 # MyTopling 企业版(包含 topling-rocks 模块) 必须配置该变量
 export ZIP_SERVER_OPTIONS="listening_ports=8090:num_threads=32"
+rm /tmp/Terark-* # 清理上次运行结束时的遗留垃圾文件
 
 #export LOG_LEVEL=3
 #export JsonOptionsRepo_DebugLevel=3
