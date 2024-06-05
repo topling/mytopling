ad214327d2b94640ccf2832506d23a23daabfcbc
leipeng <peng@topling.cn>
2024-06-05 20:03:13 +0800

    squash branch topling-8.0.28-2023-09-13-b4981a19 for logger

--- storage/rocksdb/logger.h
+++ storage/rocksdb/logger.h
@@ -15,16 +15,7 @@
    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
 #pragma once
 
-#include <string>
-
-#include "rocksdb/env.h"
-
-#include "rdb_utils.h"  // LOG_COMPONENT_TAG for includes below
-
-#include "my_loglevel.h"
-#include "mysql/components/services/log_builtins.h"
-#include "mysql/components/services/log_shared.h"
-#include "mysqld_error.h"
+#include <rocksdb/env.h> // for rocksdb::Logger
 
 namespace myrocks {
 
@@ -35,62 +26,19 @@ class Rdb_logger : public rocksdb::Logger {
       : m_mysql_log_level(log_level) {}
 
   void Logv(const rocksdb::InfoLogLevel log_level, const char *format,
-            va_list ap) override {
-    assert(format != nullptr);
+            va_list ap) override;
 
-    enum loglevel mysql_log_level;
-
-    if (m_logger) {
-      m_logger->Logv(log_level, format, ap);
-    }
-
-    if (log_level < m_mysql_log_level) {
-      return;
-    }
-
-    if (log_level >= rocksdb::InfoLogLevel::ERROR_LEVEL) {
-      mysql_log_level = ERROR_LEVEL;
-    } else if (log_level >= rocksdb::InfoLogLevel::WARN_LEVEL) {
-      mysql_log_level = WARNING_LEVEL;
-    } else {
-      mysql_log_level = INFORMATION_LEVEL;
-    }
-
-    // log to MySQL
-    std::string f("LibRocksDB:");
-    f.append(format);
-
-    // Given that we are working with a va_list, we can't pass it down
-    // to log_errlog_formatted and need to format the message ourselves
-    char msg[LOG_BUFF_MAX];
-    vsnprintf(msg, sizeof(msg) - 1, f.c_str(), ap);
-
-    LogPluginErrMsg(mysql_log_level, ER_LOG_PRINTF_MSG, "%s", msg);
-  }
-
-  void Logv(const char *format, va_list ap) override {
-    assert(format != nullptr);
-    // If no level is specified, it is by default at information level
-    Logv(rocksdb::InfoLogLevel::INFO_LEVEL, format, ap);
-  }
+  void Logv(const char *format, va_list ap) override;
 
   void SetRocksDBLogger(const std::shared_ptr<rocksdb::Logger> logger) {
     m_logger = logger;
   }
 
-  void SetInfoLogLevel(const rocksdb::InfoLogLevel log_level) override {
-    // The InfoLogLevel for the logger is used by rocksdb to filter
-    // messages, so it needs to be the lower of the two loggers
-    rocksdb::InfoLogLevel base_level = log_level;
-
-    if (m_logger && m_logger->GetInfoLogLevel() < base_level) {
-      base_level = m_logger->GetInfoLogLevel();
-    }
-    rocksdb::Logger::SetInfoLogLevel(base_level);
-    m_mysql_log_level = log_level;
-  }
+  void SetInfoLogLevel(const rocksdb::InfoLogLevel log_level) override;
 
  private:
+  rocksdb::Status CloseImpl() override;
+
   std::shared_ptr<rocksdb::Logger> m_logger;
   rocksdb::InfoLogLevel m_mysql_log_level;
 };
