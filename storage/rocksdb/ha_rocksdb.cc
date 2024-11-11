b114f2db6822b5b56ca3db00c6f343aaa7325220
rockeet <rockeet@gmail.com>
2024-11-11 23:16:22 +0800

    ha_rocksdb::generate_cf_name: restrict use whole name as cfname
    cases:
      1. cfname=XXX in key comment or table comment, cfname is XXX
      2. whole comment "rev:XXX" on key comment, cfname is "rev:XXX"
    
    comment on table without cfname will not use whole comment
    as cfname!

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -12583,7 +12583,8 @@ const std::string ha_rocksdb::generate_cf_name(uint index,
   // comment itself.  NOTE: this currently handles returning the cf name
   // specified in the index comment in the case of no partitions, which doesn't
   // use any qualifiers at the moment. (aka its a special case)
-  if (cf_name.empty() && !key_comment.empty()) {
+  if (cf_name.empty() && comment && strncmp(comment, "rev:", 4) == 0) {
+    // key_comment is from var comment(real key comment)
     auto is_cfname_char = [](const unsigned char c) {
       return isalnum(c) || c == '-' || c == '_' || c == '.';
     };
