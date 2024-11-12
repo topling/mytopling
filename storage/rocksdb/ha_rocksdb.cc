84d4a85ab479e0e8792858f1297099fa89a6b70c
rockeet <rockeet@gmail.com>
2024-11-12 12:40:52 +0800

    MyTopling: never drop cf

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -16407,6 +16407,7 @@ void Rdb_drop_index_thread::run() {
         }
       }
 
+  #if 0 // !!MyTopling: never drop cf
       DBUG_EXECUTE_IF("rocksdb_drop_cf", {
         THD *thd = new THD();
         thd->thread_stack = reinterpret_cast<char *>(&(thd));
@@ -16466,6 +16467,7 @@ void Rdb_drop_index_thread::run() {
           delete thd;
         }
       });
+  #endif
     }
     RDB_MUTEX_LOCK_CHECK(m_signal_mutex);
   }
