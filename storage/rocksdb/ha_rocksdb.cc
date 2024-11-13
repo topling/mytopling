df949c995f8ec26c355c63f2af4e4abb9c071e09
rockeet <rockeet@gmail.com>
2024-11-13 23:34:53 +0800

    Add env TOPLING_SIDEPLUGIN_CONF_MTR for work with mtr

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -134,6 +134,7 @@ void thd_mark_transaction_to_rollback(MYSQL_THD thd, int all);
 
 extern bool yield_condition(TABLE*);
 extern bool opt_binlog_ddl_only_follower; // defined in mysqld.cc
+extern uint mysqld_port; // defined in mysqld.cc
 
 /**
  *   Get the user thread's binary logging format
@@ -1240,6 +1241,22 @@ static std::shared_ptr<rocksdb::DBOptions> rdb_init_rocksdb_db_options(void) {
       fprintf(stderr, "Error ImportAutoFile(%s) = %s\n",
               side_conf, s.ToString().c_str());
       ::exit(HA_EXIT_FAILURE);
+    }
+    if (const char* side_conf_mtr = getenv("TOPLING_SIDEPLUGIN_CONF_MTR")) {
+      s = g_repo.ImportAutoFile(side_conf_mtr);
+      if (!s.ok()) {
+        fprintf(stderr, "Error ImportAutoFile(%s) for MTR = %s\n",
+                side_conf_mtr, s.ToString().c_str());
+        ::exit(HA_EXIT_FAILURE);
+      }
+      if (mysqld_port && g_repo.m_impl->http_js.contains("listening_ports")) {
+        auto http_port = std::min(mysqld_port + 2000, 65500u);
+        g_repo.m_impl->http_js["listening_ports"] = std::to_string(http_port);
+        fprintf(stderr, "INFO: sideplugin http port is %d for mtr\n", http_port);
+      }
+      if (!g_repo.m_impl->http_js.contains("listening_ports")) {
+        fprintf(stderr, "INFO: sideplugin http is disabled\n");
+      }
     }
     ROCKSDB_VERIFY_EQ(g_repo.m_impl->db_options.name2p->size(), 1);
     try {
