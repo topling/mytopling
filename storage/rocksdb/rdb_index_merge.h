4eb266530f2ec37a796a6e0a7533ca4d6bf5e837
rockeet <rockeet@gmail.com>
2024-11-16 11:48:34 +0800

    Rdb_index_merge: keep same behavior of upstream with cmp meta in cmp obj

--- storage/rocksdb/rdb_index_merge.h
+++ storage/rocksdb/rdb_index_merge.h
@@ -104,7 +104,6 @@ class Rdb_index_merge {
   struct merge_heap_entry {
     std::shared_ptr<merge_buf_info> m_chunk_info; /* pointer to buffer info */
     uchar *m_block; /* pointer to heap memory where record is stored */
-    const rocksdb::Comparator *const m_comparator;
     rocksdb::Slice m_key; /* current key pointed to by block ptr */
     rocksdb::Slice m_val;
 
@@ -121,30 +120,36 @@ class Rdb_index_merge {
                                  const uchar **block_ptr)
         MY_ATTRIBUTE((__nonnull__));
 
-    // For bulk load, use non-timestamp aware comparator because we don't assign
-    // timestamp to bulk-loaded key. GetRootComparator() can return us a
-    // non-timestamp aware one when UDT-IN-MEM is enabled or disabled.
-    explicit merge_heap_entry(const rocksdb::Comparator *const comparator)
-        : m_chunk_info(nullptr),
-          m_block(nullptr),
-          m_comparator(comparator->GetRootComparator()) {}
+    explicit merge_heap_entry(const rocksdb::Comparator*)
+        : m_chunk_info(nullptr), m_block(nullptr) {}
   };
 
+  // For bulk load, use non-timestamp aware comparator because we don't assign
+  // timestamp to bulk-loaded key. GetRootComparator() can return us a
+  // non-timestamp aware one when UDT-IN-MEM is enabled or disabled.
+  //
+  // MyTopling do not support UDT now.
   struct merge_heap_comparator {
+    merge_heap_comparator(const rocksdb::Comparator* c)
+       : m_is_rev(c->IsReverseBytewise()) {}
+    bool m_is_rev;
     bool operator()(const std::shared_ptr<merge_heap_entry> &lhs,
                     const std::shared_ptr<merge_heap_entry> &rhs) {
-    #if 0
-      return lhs->m_comparator->Compare(rhs->m_key, lhs->m_key) < 0;
-    #else
-      return rhs->m_key < lhs->m_key; // 'r < l' means '>', yield min heap
-    #endif
+      if (m_is_rev)
+        return lhs->m_key < rhs->m_key; // 'l < r' : rev min heap
+      else
+        return lhs->m_key > rhs->m_key; // 'l > r' : asc min heap
     }
   };
 
   /* Represents a record in unsorted buffer */
   struct merge_record {
     uchar *m_block; /* points to offset of key in sort buffer */
-    bool operator<(merge_record) const noexcept;
+    struct Less {
+      Less(const rocksdb::Comparator* c) : m_is_rev(c->IsReverseBytewise()) {}
+      bool m_is_rev;
+      bool operator()(merge_record x, merge_record y) const noexcept;
+    };
   };
 
   const char *m_tmpfile_path;
@@ -157,7 +162,7 @@ class Rdb_index_merge {
   struct merge_file_info m_merge_file;
   std::shared_ptr<merge_buf_info> m_rec_buf_unsorted;
   std::shared_ptr<merge_buf_info> m_output_buf;
-  std::set<merge_record> m_offset_tree;
+  std::set<merge_record, merge_record::Less> m_offset_tree;
   std::priority_queue<std::shared_ptr<merge_heap_entry>,
                       std::vector<std::shared_ptr<merge_heap_entry>>,
                       merge_heap_comparator>
