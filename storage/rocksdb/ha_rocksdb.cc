6cab591c0d312fb72cde00a78f81c353e6542947
rockeet <rockeet@gmail.com>
2024-12-08 09:40:41 +0800

    ha_rocksdb.cc: minor fix error msg

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -4549,14 +4549,14 @@ class Rdb_transaction {
         std::error_code ec;
         std::filesystem::remove_all(dir, ec);
         if (ec.value() == (int)std::errc::no_such_file_or_directory) {
-          sql_print_information("remove_all(%s) = no_such_file_or_directory",
+          sql_print_information("MyRocks: remove_all(%s) = no_such_file_or_directory",
                                  dir.c_str());
         }
         else if (ec.value() == 0) {
           // sql_print_information("remove_all(%s) = success", dir.c_str());
         }
         else {
-          sql_print_error("remove_all(%s) = %s", dir.c_str(), strerror(ec.value()));
+          sql_print_error("MyRocks: remove_all(%s) = %s", dir.c_str(), strerror(ec.value()));
         }
       }
     });
@@ -13282,14 +13282,14 @@ int ha_rocksdb::finalize_bulk_load(bool print_client_error) {
           std::error_code ec;
           std::filesystem::remove_all(dir, ec);
           if (ec.value() == (int)std::errc::no_such_file_or_directory) {
-            sql_print_information("remove_all(%s) = no_such_file_or_directory",
+            sql_print_information("MyRocks: remove_all(%s) = no_such_file_or_directory",
                                   dir.c_str());
           }
           else if (ec.value() == 0) {
             // sql_print_information("remove_all(%s) = success", dir.c_str());
           }
           else {
-            sql_print_error("remove_all(%s) = %s", dir.c_str(), strerror(ec.value()));
+            sql_print_error("MyRocks: remove_all(%s) = %s", dir.c_str(), strerror(ec.value()));
           }
         });
         auto files = commit_info.get_committed_files();
