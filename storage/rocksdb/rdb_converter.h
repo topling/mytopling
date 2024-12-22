34fb9be21e0d0e169c908a0567141fe94d8a6c86
rockeet <rockeet@gmail.com>
2024-12-22 12:52:02 +0800

    Fix Rdb_converter::decode() for mtr corrupted_data_reads_debug

--- storage/rocksdb/rdb_converter.h
+++ storage/rocksdb/rdb_converter.h
@@ -170,7 +170,7 @@ class Rdb_converter {
   int decode(const std::shared_ptr<Rdb_key_def> &key_def, uchar *dst,
              const rocksdb::Slice *key_slice, const rocksdb::Slice *value_slice,
              bool decode_value = true) {
-    return value_slice->empty() && !m_has_instant_fields ?
+    return value_slice->empty() && !m_needs_kv_value ?
       decode_tpl<Rdb_empty_reader>(key_def.get(), dst, key_slice, nullptr, decode_value) :
       decode_tpl<Rdb_string_reader>(key_def.get(), dst, key_slice, value_slice, decode_value);
   }
