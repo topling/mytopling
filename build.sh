09e945a3bc90e1c5ca115c6e19e2164385cc1b19
rockeet <rockeet@gmail.com>
2024-12-24 22:18:58 +0800

    build.sh: Add -Wno-dev

--- build.sh
+++ build.sh
@@ -55,4 +55,5 @@ cmake -DHAVE_EXTERNAL_ROCKSDB=1 -DROCKSDB_SRC_PATH=${PDIR}/toplingdb \
       -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
       -DCMAKE_INSTALL_PREFIX=/opt/mytopling-rls \
       -DMYSQL_UNIX_ADDR=/var/lib/mysql/mysql.sock \
+      -Wno-dev \
       "$@" ..
