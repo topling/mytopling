e874da98abee59be338464ef3662198aa5d21b78
leipeng <peng@topling.cn>
2025-04-01 16:56:56 +0800

    rdb_sst_info.cc: Rdb_sst_info::~Rdb_sst_info: minor fix: remove false warn log

--- storage/rocksdb/rdb_sst_info.cc
+++ storage/rocksdb/rdb_sst_info.cc
@@ -388,7 +388,8 @@ Rdb_sst_info::Rdb_sst_info(rocksdb::DB *db, const std::string &tablename,
 Rdb_sst_info::~Rdb_sst_info() {
   for (auto& [index_id, sst] : m_sst_map) {
     //SHIP_ASSERT(m_sst_file == nullptr);
-    sql_print_warning("index_id %d, sst file %s",
+    if (m_sst_file)
+      sql_print_warning("index_id %d, sst file %s",
         index_id, m_sst_file ? m_sst_file->get_name().c_str() : "(null)");
   }
 
